7cdae0a755a90b9ddf5e88f00727f419812b4f46
Konstantinos Egkarchos <konsnosl@gmail.com>
2014-08-19 00:36:29 +0300

    Commit containing the whole project.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ ShaderLOD
 =========
 
 Shader LOD example.
+
+Contains a scene showing how Shader LOD is used. The scene automatically drops the used Shader LOD showing 2 objects and how they differ from each other for each LOD by using a different SubShader for each LOD set for the shader.
---
+++ ShaderLOD/Assets/Scripts/LOD.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LOD : MonoBehaviour
+{
+		//These are the builtin LOD values for Unity shaders.
+		int[] shaderLOD = new int[] { 600, 500, 400, 300, 250, 200, 150, 100 };
+	
+		void Start ()
+		{
+				StartCoroutine ("UpdateLOD");
+		}
+	
+		void SetLOD (int LOD)
+		{
+				Debug.Log ("New global Maximum LOD is " + shaderLOD [LOD]);
+				/*foreach (var i in renderer.sharedMaterials) {
+						i.shader.maximumLOD = shaderLOD [LOD];
+				}*/
+				Shader.globalMaximumLOD = shaderLOD [LOD];
+		}
+	
+		IEnumerator UpdateLOD ()
+		{
+				for (int i = 0; i<shaderLOD.Length; i++) {
+						SetLOD (i);
+						yield return new WaitForSeconds (2f);
+				}
+		}
+	
+}
